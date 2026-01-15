0c03931f03694b59adee2047c77cf93634b04d63
Myra Sidhu <myra.29@Myras-MacBook-Air.local>
2026-01-14 16:14:57 -0800

    creating intake subsystem

---
+++ src/main/java/frc/robot/commands/Divider.java
@@ -0,0 +1,21 @@
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+
+public class Divider extends CommandBase {
+        private final Fuel fuelSubsystem;
+        public Intake(fuelSubsystem){
+            this.fuelSubsystem = fuelSubsystem;
+            addRequirements(finalSubsystem);
+        }
+
+    
+    execute(){
+        
+    }
+
+
+    ifFinished(){
+
+    }
+}
