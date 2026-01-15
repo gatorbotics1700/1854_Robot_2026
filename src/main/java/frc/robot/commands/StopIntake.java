84c6ee64589a8ad337162c5387564077ec6cf5e0
Caitlyn Lee <caitlynlee@Caitlyns-MacBook-Air.local>
2026-01-14 17:02:47 -0800

    wrote StopIntake command

--- src/main/java/frc/robot/commands/StopIntake.java
+++ src/main/java/frc/robot/commands/StopIntake.java
@@ -1,22 +1,21 @@
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.intake.Intake;
 
 
-public class Intake extends Command{
+public class StopIntake extends Command{
 
     private final Intake intakeSubsystem;
     
-    
-    public Intake(Intake intakeSubsystem){
-        this.intakeSubsystem = intakeSubsystem;
+    public StopIntake(Intake fuelSubsystem){
+        this.intakeSubsystem = fuelSubsystem;
         addRequirements(intakeSubsystem);
     }
 
    @Override
    public void execute(){
-        intakeSubsystem.moveFuelMotor(Constants.FUEL_MOTOR_VOLTAGE);
+        intakeSubsystem.moveFuelMotor(0.0);
+        intakeSubsystem.moveDeployMotor(0.0);
     // set motor make an instance of. a motor and then set motor volatge to intake a certain volatge make a constant in the constants doc of that volatge
     // to set mother volt us set motor volateg (volatge)
 
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -24,4 +24,6 @@ public class Intake extends SubsystemBase{
         deployMotor.setVoltage(voltage);
     }
 
+    
+
 }
