5d5caad5ca2c9c6712921fd7600388a719197270
Violet Chaffee <26vchaffee@castilleja.org>
2026-01-14 16:14:38 -0800

    added a version of photonlib to attempt to build code, fixed some other build errors

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "1854_Robot_2026-Imported";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "ea88e93699a6bfc9d9c4e34769183b9918612e35";
+  public static final String GIT_DATE = "2026-01-13 21:51:05 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-14 19:01:46 EST";
+  public static final long BUILD_UNIX_TIME = 1768435306917L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
