61fa19506fcfb677ac499a827ad7870718961d42
Simra Paranjpye <simraparanjpye@Simras-MacBook-Air.local>
2026-01-28 18:45:56 -0800

    edits from code review

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "1854_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "af7a641d9ed19511980878bc31c7b8087882aa51";
-  public static final String GIT_DATE = "2026-01-28 18:52:11 EST";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "3b91f3c5951c136aa1c21a76e1497771228ded93";
+  public static final String GIT_DATE = "2026-01-28 20:18:26 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-28 20:11:21 EST";
-  public static final long BUILD_UNIX_TIME = 1769649081657L;
+  public static final String BUILD_DATE = "2026-01-28 21:37:53 EST";
+  public static final long BUILD_UNIX_TIME = 1769654273612L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
