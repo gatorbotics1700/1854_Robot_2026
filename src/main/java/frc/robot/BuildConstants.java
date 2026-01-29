9fd1ea4f9a9ee78714b9e82c8be06104187def77
pabeekm <patricia.beekmanjunk@gmail.com>
2026-01-28 22:58:50 -0800

    Fix simgui bindings for keyboard

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "1854_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 61;
-  public static final String GIT_SHA = "61fa19506fcfb677ac499a827ad7870718961d42";
-  public static final String GIT_DATE = "2026-01-28 21:45:56 EST";
+  public static final int GIT_REVISION = 62;
+  public static final String GIT_SHA = "dfa1b051481e3df009b48755044e732ac84f0d94";
+  public static final String GIT_DATE = "2026-01-29 01:36:52 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-29 01:31:40 EST";
-  public static final long BUILD_UNIX_TIME = 1769668300533L;
+  public static final String BUILD_DATE = "2026-01-29 01:51:27 EST";
+  public static final long BUILD_UNIX_TIME = 1769669487793L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
